af5f7d1d5f1e9b126bda25b950083243e2a731c8
Matthias J. Kannwischer <matthias@kannwischer.eu>
2023-08-28 13:49:20 +0800

    update to standard version

--- kyber/patches/ref_indcpa.c
+++ kyber/patches/ref_indcpa.c
@@ -1,6 +1,6 @@
 --- upstream/ref/indcpa.c
 +++ upstream-patched/ref/indcpa.c
-@@ -171,9 +171,9 @@
+@@ -168,9 +168,9 @@
    for(i=0;i<KYBER_K;i++) {
      for(j=0;j<KYBER_K;j++) {
        if(transposed)
@@ -12,7 +12,7 @@
  
        xof_squeezeblocks(buf, GEN_MATRIX_NBLOCKS, &state);
        buflen = GEN_MATRIX_NBLOCKS*XOF_BLOCKBYTES;
-@@ -187,6 +187,7 @@
+@@ -184,6 +184,7 @@
          buflen = off + XOF_BLOCKBYTES;
          ctr += rej_uniform(a[i].vec[j].coeffs + ctr, KYBER_N - ctr, buf, buflen);
        }
